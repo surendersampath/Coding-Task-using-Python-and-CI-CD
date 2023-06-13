791cc0720945958f59d4fafe2b748e3d38984fc6
surendersampath <surendersampath@outlook.com>
2023-06-14 00:17:02 +0100

    + Code formatting.

--- boardingpassfinder.py
+++ boardingpassfinder.py
@@ -1,4 +1,3 @@
-
 def test_func(a, b):
     return a + b
 
@@ -8,17 +7,17 @@ def decode_seat_number_from_boarding_pass(boarding_pass_string):
     row_string = boarding_pass_string[:7]
     row_binary = row_string.replace("F", "0").replace("B", "1")
     row_number = int(row_binary, 2)
-    
+
     # Decode the column number
     col_string = boarding_pass_string[7:]
     col_binary = col_string.replace("L", "0").replace("R", "1")
     col_number = int(col_binary, 2)
-    
+
     # Calculate the seat ID
     seat_id = row_number * 8 + col_number
 
     return seat_id
 
 
-def find_seat_from_scanned_boarding_pass ( scanned_boarding_pass_data ):
+def find_seat_from_scanned_boarding_pass(scanned_boarding_pass_data):
     return -1
