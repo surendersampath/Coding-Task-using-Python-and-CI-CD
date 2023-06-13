cafd3f04c64128bd2520be456be34b36b82e3a6b
surendersampath <surendersampath@outlook.com>
2023-06-14 00:41:26 +0100

    + Formatted.

--- boardingpassfinder.py
+++ boardingpassfinder.py
@@ -1,11 +1,14 @@
 import logging
 
-logging.basicConfig(filename='coding_Challenge.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename="coding_Challenge.log",
+    filemode="w",
+    format="%(name)s - %(levelname)s - %(message)s",
+)
 logging.getLogger().setLevel(logging.INFO)
 
 
 def decode_seat_number_from_boarding_pass(boarding_pass_string):
-
     if boarding_pass_string == "":
         logging.critical("Incorrect boarding pass data.")
 
@@ -31,18 +34,25 @@ def find_seat_from_scanned_boarding_pass(scanned_boarding_pass_data):
         highest_seat_id = 0
         missing_seat_id = 0
 
-        seats = set(map(decode_seat_number_from_boarding_pass, scanned_boarding_pass_data))
+        seats = set(
+            map(decode_seat_number_from_boarding_pass, scanned_boarding_pass_data)
+        )
         highest_seat_id = max(seats)
 
         # Find the missing seat by checking consecutive seat IDs
-        missing_seat_id = next(filter(lambda seat_id: seat_id not in seats and (seat_id + 1) in seats and (seat_id - 1) in seats,
-                                range(highest_seat_id + 1)))
+        missing_seat_id = next(
+            filter(
+                lambda seat_id: seat_id not in seats
+                and (seat_id + 1) in seats
+                and (seat_id - 1) in seats,
+                range(highest_seat_id + 1),
+            )
+        )
 
         logging.info("Sanity Check - Highest Seat Number : " + str(highest_seat_id))
         logging.info("Your Seat Number : " + str(missing_seat_id))
-    
+
     except:
         logging.critical("Error in Scanned Data.")
 
-    
     return missing_seat_id
