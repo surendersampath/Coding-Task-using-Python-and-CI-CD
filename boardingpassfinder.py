555595a673500887109fe9495937fa81e436f00c
surendersampath <surendersampath@outlook.com>
2023-06-14 00:01:23 +0100

    + Added test cases to fail.

--- boardingpassfinder.py
+++ boardingpassfinder.py
@@ -4,4 +4,17 @@ def test_func(a, b):
 
 
 def decode_seat_number_from_boarding_pass(boarding_pass_string):
-    return -1
+    # Decode the row number
+    row_string = boarding_pass_string[:7]
+    row_binary = row_string.replace("F", "0").replace("B", "1")
+    row_number = int(row_binary, 2)
+    
+    # Decode the column number
+    col_string = boarding_pass_string[7:]
+    col_binary = col_string.replace("L", "0").replace("R", "1")
+    col_number = int(col_binary, 2)
+    
+    # Calculate the seat ID
+    seat_id = row_number * 8 + col_number
+
+    return seat_id
--- unittests.py
+++ unittests.py
@@ -4,11 +4,21 @@ from boardingpassfinder import test_func
 
 class MyModuleTestCase(unittest.TestCase):
 
+    def testcase_seatnumber_1(self):
+        result = get_seat_number_from_boarding_pass("BFFFBBFRRR")
+        self.assertEqual(result, 567)
 
-    def testcase_1(self):
-        result = test_func(2,4)
-        self.assertEqual(result, 6)
+    def testcase_seatnumber_2(self):
+        result = get_seat_number_from_boarding_pass("FFFBBBFRRR")
+        self.assertEqual(result, 119)
 
+    def testcase_seatnumber_3(self):
+        result = get_seat_number_from_boarding_pass("BBFFBBFRLL")
+        self.assertEqual(result, 820)
+
+    def testcase_seatnumber_4(self):
+        result = get_seat_number_from_boarding_pass("FBFBBFFRLR")
+        self.assertEqual(result, 357)
 
 if __name__ == '__main__':
     unittest.main()
