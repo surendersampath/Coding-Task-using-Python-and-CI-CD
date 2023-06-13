d26f19a866d4020370c288d5a157b98685115d4b
surendersampath <surendersampath@outlook.com>
2023-06-14 00:06:58 +0100

    + Updated unit tests.

--- unittests.py
+++ unittests.py
@@ -5,19 +5,19 @@ from boardingpassfinder import test_func
 class MyModuleTestCase(unittest.TestCase):
 
     def testcase_seatnumber_1(self):
-        result = get_seat_number_from_boarding_pass("BFFFBBFRRR")
+        result = decode_seat_number_from_boarding_pass("BFFFBBFRRR")
         self.assertEqual(result, 567)
 
     def testcase_seatnumber_2(self):
-        result = get_seat_number_from_boarding_pass("FFFBBBFRRR")
+        result = decode_seat_number_from_boarding_pass("FFFBBBFRRR")
         self.assertEqual(result, 119)
 
     def testcase_seatnumber_3(self):
-        result = get_seat_number_from_boarding_pass("BBFFBBFRLL")
+        result = decode_seat_number_from_boarding_pass("BBFFBBFRLL")
         self.assertEqual(result, 820)
 
     def testcase_seatnumber_4(self):
-        result = get_seat_number_from_boarding_pass("FBFBBFFRLR")
+        result = decode_seat_number_from_boarding_pass("FBFBBFFRLR")
         self.assertEqual(result, 357)
 
 if __name__ == '__main__':
