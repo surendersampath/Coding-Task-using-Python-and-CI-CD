98c72b2aa79eac2f133b766fb1c02171886824ba
surendersampath <surendersampath@outlook.com>
2023-06-14 00:44:53 +0100

    + Added comments.

--- boardingpassfinder.py
+++ boardingpassfinder.py
@@ -1,5 +1,6 @@
 import logging
 
+# Configure logging settings
 logging.basicConfig(
     filename="coding_Challenge.log",
     filemode="w",
@@ -9,26 +10,43 @@ logging.getLogger().setLevel(logging.INFO)
 
 
 def decode_seat_number_from_boarding_pass(boarding_pass_string):
+    """
+    Decode the seat number from a boarding pass string.
+    Args:
+        boarding_pass_string (str): The boarding pass string.
+
+    Returns:
+        int: The decoded seat number.
+    """
     if boarding_pass_string == "":
         logging.critical("Incorrect boarding pass data.")
+        return 0
 
-    # Decode the row number
+    # Decode the row number.
     row_string = boarding_pass_string[:7]
     row_binary = row_string.replace("F", "0").replace("B", "1")
     row_number = int(row_binary, 2)
 
-    # Decode the column number
+    # Decode the column number.
     col_string = boarding_pass_string[7:]
     col_binary = col_string.replace("L", "0").replace("R", "1")
     col_number = int(col_binary, 2)
 
-    # Calculate the seat ID
+    # Calculate the seat ID as per the formula given to generate UNIQUE seat ID.
     seat_id = row_number * 8 + col_number
 
     return seat_id
 
 
 def find_seat_from_scanned_boarding_pass(scanned_boarding_pass_data):
+    """
+    Find the missing seat number from a list of scanned boarding pass data.
+    Args:
+        scanned_boarding_pass_data (set): The list of scanned boarding pass data.
+
+    Returns:
+        int: The missing seat number.
+    """
     try:
         seats = set()
         highest_seat_id = 0
@@ -39,7 +57,7 @@ def find_seat_from_scanned_boarding_pass(scanned_boarding_pass_data):
         )
         highest_seat_id = max(seats)
 
-        # Find the missing seat by checking consecutive seat IDs
+        # Find the missing seat by checking consecutive seat ID's.
         missing_seat_id = next(
             filter(
                 lambda seat_id: seat_id not in seats
